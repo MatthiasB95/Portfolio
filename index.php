f0de221292b5fcf728266915bf7a70f1a6a16cf7
unknown <mbruckbeck@gmx.at>
2019-08-22 16:05:12 +0200

    added contactform.php to create email contact form

---
+++ index.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Matthias Bruckbeck Portfolio</title>
+<!--
+Stacked Template
+http://www.templatemo.com/tm-505-stacked
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <nav class="nav">
+          <div class="burger">
+            <div class="burger__patty"></div>
+          </div>
+
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="#1" class="nav__link c-blue"><img src="img/home-icon.png" alt=""></a>
+            </li>
+            <li class="nav__item">
+              <a href="#2" class="nav__link c-yellow scrolly"><img src="img/about-icon.png" alt=""></a>
+            </li>
+            <li class="nav__item">
+              <a href="#3" class="nav__link c-red"><img src="img/projects-icon.png" alt=""></a>
+            </li>
+            <li class="nav__item">
+              <a href="#4" class="nav__link c-green"><img src="img/contact-icon.png" alt=""></a>
+            </li>
+          </ul>
+        </nav>
+
+        <section class="panel b-blue" id="1">
+          <article class="panel__wrapper">
+            <div class="panel__content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div class="home-content">
+                      <div class="home-heading">
+                        <h1><em>Welcome to my Portfolio</em></h1>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="home-box-content">
+                            <div class="left-text">
+                              <h4>Matthias<em> Bruckbeck</em></h4>
+                              <p>I am 24 years old and currently looking for a job in Web Developing. On this Webiste I would like to tell you a bit more about me and show you some of my recent projects I worked on alone or with team members.<br>
+                              If you want to know more about me take a look at my LinkedIn profile.</p>
+                              <div class="primary-button">
+                                <a href="https://at.linkedin.com/in/matthias-bruckbeck-264737191?trk=people-guest_profile-result-card_result-card_full-click" target="_blank">LinkedIn</a>
+                              </div>
+                            </div>
+                            <div class="right-image">
+                              <img id="profile_img" src="img/profile_img.jpg" style="height: 15em;" alt="">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </section>
+        <section class="panel b-yellow" id="2">
+          <article class="panel__wrapper">
+            <div class="panel__content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div class="about-content">
+                      <div class="heading">
+                        <h4>About me</h4>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12 col-sm-2 ">
+                          <div class="about-box-content">
+                            <img class="d-none d-sm-block" src="img/mycode.png" alt="">
+                          </div>
+                        </div>
+                        <div class="col-md-7 col-md-offset-5 col-sm-7 col-sm-offset-5" style="height: 100%;">
+                          <div class="about-box-text">
+                            <h4>Some Personal Information</h4>
+                            <p>I just recently graduated in a Full-Stack Web Development Course with Summa cum laude. The Course included for example HTML, CSS, PHP, JavaScript and many more topics. I also gained a lot of experience about working on projects for a longer time alone as well as in a team of three or four people.<br> If you would like to know more please click on the button below to see my CV.</p>
+                            <div class="primary-button">
+                              <a href="cv/CV.docx" target="_blank">CV German</a>
+                              <a href="cv/CV-English.docx" target="_blank">CV English</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </section>
+        <section class="panel b-red" id="3">
+          <article class="panel__wrapper">
+            <div class="panel__content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div class="projects-content">
+                      <div class="heading">
+                        <h4>Recent Projects</h4>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="owl-carousel owl-theme projects-container">
+                            <div>
+                              <div class="project-item col-sm-6">
+                                <a href="img/last-project.png" data-lightbox="image-1"><img src="img/last-project.png" alt=""></a>
+                                <div class="text-content">
+                                  <h4>Final Project</h4>
+                                  <p>Together with three team members we decided to create a Website for sport enthusiasts where people are able to find other people who share their interests. The Website is supposed to let the users create groups and join together for events.</p>
+                                  <div class="primary-button">
+                                    <a href="https://github.com/CodeFactoryWien/FSWD70-Final-Project-teamtwo" target= "_blank">See more on my Github</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <div class="project-item">
+                                <a href="img/coretp-team2.png" data-lightbox="image-1"><img src="img/coretp-team2.png" alt=""></a>
+                                <div class="text-content">
+                                  <h4>Core Technologies Project</h4>
+                                  <p>This is my first project which I did with three team members in one week. the task was to create a set of web pages that describe our team, and our team members.</p>
+                                  <div class="primary-button">
+                                    <a href="https://github.com/CodeFactoryWien/FSWD7-CoreTP-Team2" target="_blank">See more on my Github</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <div class="project-item">
+                                <a href="img/cr13.png" data-lightbox="image-1"><img src="img/cr13.png" alt=""></a>
+                                <div class="text-content">
+                                  <h4>Symfony</h4>
+                                  <p>For this project I used Symfony and PHP. The task I was given here was that a global event management company needs an application to track events. It is one of the projects I did alone while doing the Full-Stack course.</p>
+                                  <div class="primary-button">
+                                    <a href="https://github.com/MatthiasB95/FSWD70-Matthias-Bruckbeck-CodeReview13">See more on my Github</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <div class="project-item">
+                                <a href="img/cr12.png" data-lightbox="image-1"><img src="img/cr12.png" alt=""></a>
+                                <div class="text-content">
+                                  <h4>WordPress theme</h4>
+                                  <p>For this project I created my own WordPress theme. After a week of learning how to use WordPress I started to work on my own theme during the weekend. I created a WordPress theme with Bootstrap which included widgets and a menu with a dropdown.</p>
+                                  <div class="primary-button">
+                                    <a href="https://github.com/MatthiasB95/FSWD70-CodeReview12-Matthias">See more on my Github</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <div class="project-item">
+                                <a href="img/cr11.png" data-lightbox="image-1"><img src="img/cr11.png" alt=""></a>
+                                <div class="text-content">
+                                  <h4>API</h4>
+                                  <p>For this project I reused an already finished project I did about two months ago. So this time I implemented parts from the Back-End themes we were learning about. I added a database and got the information shown on the Website from there. I also added some Google maps APIs.</p>
+                                  <div class="primary-button">
+                                    <a href="https://github.com/MatthiasB95/FSWD70-CodeReview11-Matthias">See more on my Github</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <div class="project-item">
+                                <a href="img/cr10.png" data-lightbox="image-1"><img src="img/cr10.png" alt=""></a>
+                                <div class="text-content">
+                                  <h4>PHP and MySQL</h4>
+                                  <p>This was the first time I used PHP and MySQL at the same time for a project. I created a database from scratch and later on I added the PHP part to display all the necessary information from the database without writing them directly into my code.</p>
+                                  <div class="primary-button">
+                                    <a href="https://github.com/MatthiasB95/FSWD70-CodeReview10-Matthias">See more on my Github</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </section>
+        <section class="panel b-green" id="4">
+          <article class="panel__wrapper">
+            <div class="panel__content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div class="contact-content">
+                      <div class="heading">
+                        <h4>Contact me</h4>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-8">
+                          <div class="contat-form">
+                            <form id="contact" class="contact-form" action="https://formspree.io/mbruckbeck@gmx.at" method="post">
+                              <fieldset>
+                                <input name="name" type="text" class="form-control" id="name" placeholder="Your Name" required>
+                              </fieldset>
+                              <fieldset>
+                                <input name="email" type="email" class="form-control" id="email" placeholder="Your Email" required>
+                              </fieldset>
+                              <fieldset>
+                                <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject" required>
+                              </fieldset>
+                              <fieldset>
+                                <textarea name="message" rows="8" cols="80" class="form-control" id="message" placeholder="Message" required></textarea>
+                              </fieldset>
+                              <fieldset>
+                                <button type="submit" id="form-submit" name="submit">Send Message</button>
+                              </fieldset>
+                            </form>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="more-info text-content">
+                            <p>For further information about me or my work feel free to contact me via email.<br> If you would like to see more of my work please visit my Github Account.</p>
+                            <div class="primary-button">
+                              <a href="https://github.com/MatthiasB95?tab=repositories" target="_blank">Github</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="footer">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </section>
+      
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
